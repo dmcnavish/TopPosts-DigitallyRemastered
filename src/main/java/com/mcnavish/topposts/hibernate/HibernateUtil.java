0b245fe0d47d51461587b291151deb114f97fd5b
dmcnavish <dave8051@gmail.com>
2015-04-27 19:55:42 -0700

    updated open shift build and start scripts

--- src/main/java/com/mcnavish/topposts/hibernate/HibernateUtil.java
+++ src/main/java/com/mcnavish/topposts/hibernate/HibernateUtil.java
@@ -1,35 +1,26 @@
 package com.mcnavish.topposts.hibernate;
 
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
-
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//@Component
 public class HibernateUtil {
 	
 	private static Logger logger = LoggerFactory.getLogger(HibernateUtil.class);
 	private static final SessionFactory sessionFactory;
-	
-//	@Autowired
-//	private static DataSource dataSource;
-	
+
 	static{
+
 		Configuration configuration = new Configuration().configure();
 		String connectionHost = System.getenv("OPENSHIFT_POSTGRESQL_DB_HOST");
 		String connectionPort = System.getenv("OPENSHIFT_POSTGRESQL_DB_PORT");
 		String databaseName = "/topposts";
-		boolean isOpenShift = true;
 		
 		if(connectionHost == null || connectionHost.isEmpty() || connectionPort == null || connectionPort.isEmpty()){
 			connectionHost = "localhost";
 			connectionPort = "5432";
-			isOpenShift = false;
 		}
 
 		String connectionUrl = "jdbc:postgresql://" + connectionHost + ":" + connectionPort + databaseName;
@@ -45,30 +36,12 @@ public class HibernateUtil {
 		logger.debug("connectionUrl: " + connectionUrl);
 		logger.debug("connectionUserName: " + connectionUserName);
 		logger.debug("connectionPassword: " + connectionPassword);
-		
-		if(isOpenShift){
-//			configuration.setProperty("hibernate.connection.url", "");
-			configuration.setProperty("hibernate.connection.datasource","java:comp/env/jdbc/PostgreSQLDS");
-		}
-		else{
-			configuration.setProperty("hibernate.connection.url", connectionUrl);
-			configuration.setProperty("hibernate.connection.username", connectionUserName);
-			configuration.setProperty("hibernate.connection.password", connectionPassword);
-		}
 
+		configuration.setProperty("hibernate.connection.url", connectionUrl);
+		configuration.setProperty("hibernate.connection.username", connectionUserName);
+		configuration.setProperty("hibernate.connection.password", connectionPassword);
 		
 		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
-//		
-//		if(isOpenShift){
-//			try{
-//				DataSource dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/PostgreSQLDS");
-//				builder.applySetting(Environment.DATASOURCE, dataSource);
-//			}catch(Exception ex){
-//				logger.error("Error setting datasource", ex);
-//			}
-//		}
-		
-		
 		sessionFactory = configuration.buildSessionFactory(builder.build());
 	}
 	
